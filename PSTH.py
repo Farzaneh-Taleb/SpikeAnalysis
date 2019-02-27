1756bc53f7cbad0cc4be31f179d522379cbe7fcc
farzaneh <farzaneh.taleb@ut.ac.ir>
2019-02-27 11:22:03 +0330

    reformat code

--- PSTH.py
+++ PSTH.py
@@ -1,4 +1,3 @@
-
 """
 @author Farzaneh.Tlb
 1/30/2019 1:07 AM
@@ -11,26 +10,28 @@ import matplotlib.pyplot as plt
 Nu = loadmat('../datas/Nu.mat')
 conNU = loadmat('../datas/conNU.mat')
 
-Nu_data = Nu['nu'] #(25,928,3500)
-conNU_data = conNU['condNU'] #(928, 1)
+Nu_data = Nu['nu']  # (25,928,3500)
+conNU_data = conNU['condNU']  # (928, 1)
 
 neurons = Nu_data[0]
-neurons_array = np.empty(shape = [25,928,3500])
+neurons_array = np.empty(shape=[25, 928, 3500])
 trial_number = 3500
-
 
 for index, neuron in np.ndenumerate(neurons):
     neurons_array[index] = neuron
 
-def getTrialByCondition(neuron_number , condition):
-    s = neurons_array[neuron_number,(conNU_data==condition).squeeze() , :]
-    data = np.sum(s , axis=0 )
+
+def getTrialByCondition(neuron_number, condition):
+    s = neurons_array[neuron_number, (conNU_data == condition).squeeze(), :]
+    data = np.sum(s, axis=0)
     print(s.shape)
     print(data)
 
     # return dict(zip(unique, counts))
-    return data , s.shape[0]
-summation , number_of_trials =getTrialByCondition(11, 1)
+    return data, s.shape[0]
+
+
+summation, number_of_trials = getTrialByCondition(11, 1)
 hist = summation / number_of_trials
-plt.plot(hist,'-')
+plt.plot(hist, '-')
 plt.show()
--- RasterPlot1.py
+++ RasterPlot1.py
@@ -1,6 +1,7 @@
 import utility_functions as utility
 import matplotlib.pyplot as plt
 import numpy as np
+
 """
 @author Farzaneh.Tlb
 2/27/19 12:49 AM
@@ -10,23 +11,23 @@ Implementation of RasterPlot(Fill this line)
 ut = utility.Utility_Functions()
 trials = 20
 
-
-array_color = ['green' , 'red' , 'blue' , 'yellow','black' , 'orange' , 'gray' , 'red']
+array_color = ['green', 'red', 'blue', 'yellow', 'black', 'orange', 'gray', 'red']
 for n in range(ut.number_of_neurons):
-    for  c in range(ut.conditions-8):
-        trials = ut.get_trial_by_condition(n,c+1)
-        selected_trials = trials[0:20,:]
+    for c in range(ut.conditions - 8):
+        trials = ut.get_trial_by_condition(n, c + 1)
+        selected_trials = trials[0:20, :]
         # for index, trial in np.ndenumerate(selected_trials):
         trial_index = 1
         for trial in selected_trials:
             spikes = np.nonzero(trial)
             print(spikes[0].size)
-            plt.scatter(spikes,np.ones(spikes[0].size)*10*(c+1) + trial_index/2 , marker='.',edgecolors=array_color[c]  , s=1 )
-            x = 10*(c+1) + trial_index
-            trial_index +=1
-            print("x" ,x)
+            plt.scatter(spikes, np.ones(spikes[0].size) * 10 * (c + 1) + trial_index / 2, marker='.',
+                        edgecolors=array_color[c], s=1)
+            x = 10 * (c + 1) + trial_index
+            trial_index += 1
+            print("x", x)
 
-    plt.axhline(y=10 )
+    plt.axhline(y=10)
     plt.axhline(y=20)
     plt.axhline(y=30)
     plt.axhline(y=40)
@@ -37,6 +38,7 @@ for n in range(ut.number_of_neurons):
     plt.legend()
     y_axis = np.arange(10, 90, 10)
 
-    plt.yticks(y_axis, ['condition 1' ,'condition 2','condition 3','condition 4','condition 5','condition 6','condition 7','condition 8'])
+    plt.yticks(y_axis, ['condition 1', 'condition 2', 'condition 3', 'condition 4',
+                        'condition 5', 'condition 6', 'condition 7', 'condition 8'])
 
     plt.show()
