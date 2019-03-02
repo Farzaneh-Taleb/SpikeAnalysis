063e62b1ce7c4ff5e359bfae910bae5b7a6d47ce
farzaneh <farzaneh.taleb@ut.ac.ir>
2019-03-03 01:44:48 +0330

    classifier

--- classifier.py
+++ classifier.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import  accuracy_score
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score
+
 
 
 
@@ -21,26 +23,21 @@ all_Y = np.where(all_Y <= 8 , all_Y, all_Y-8)
 for n  in range(ut.number_of_neurons):
     neuron = ut.get_neuron(n)
     all_X[:,n] = np.transpose(ut.compute_summation_over_time(neuron))
-    # print("n" , n  ,all_X)
 
-# print(all_X.shape)
-# print(all_Y.shape)
-
-# X_train, X_test, y_train, y_test = train_test_split( all_X, all_Y, test_size=0.3)
-# print(X_train.shape)
-# print(X_test.shape)
-# print(y_train.shape)
-# print(y_test.shape)
-# clf = svm.SVC(kernel='linear' , decision_function_shape='ovr' ).fit(X_train,y_train)
-# y  =clf.predict(X_test)
-# print(accuracy_score(y , y_test))
 
 
 kf = KFold(n_splits=4)
 
-for train , test  in kf.split(all_X ,all_Y):
-    X_train, X_test, y_train, y_test = all_X[ train], all_X[ test], all_Y[train], all_Y[test]
+# for train , test  in kf.split(all_X ,all_Y):
+#     X_train, X_test, y_train, y_test = all_X[ train], all_X[ test], all_Y[train], all_Y[test]
+#
+#     clf = svm.SVC(kernel='linear', decision_function_shape='ovr').fit(X_train, y_train)
+#     y  =clf.predict(X_test)
+#     print(accuracy_score(y, y_test))
 
-    clf = svm.SVC(kernel='linear', decision_function_shape='ovr').fit(X_train, y_train)
-    y  =clf.predict(X_test)
-    print(accuracy_score(y, y_test))
+clf = svm.SVC(kernel='linear', decision_function_shape='ovr')
+scores = cross_val_score(clf, all_X, all_Y, cv=5)
+print(scores)
+print("Accuracy:"  , scores.mean(),"(+/-" , scores.std() * 2 , ")")
+print(scores.mean() +  scores.std() * 2 , scores.mean() -  scores.std() * 2)
+
